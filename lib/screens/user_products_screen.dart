33b1697cb79e32cfe28689ca5d7be8311852d270
ajitaz <azalbert.ab@gmail.com>
2021-07-02 18:41:28 +0545

    Edit product screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../widgets/app_drawer.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-Product';
@@ -11,11 +12,13 @@ class UserProductsScreen extends StatelessWidget {
     final productData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Products'),
+        title: const Text('Your Products'),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+            icon: const Icon(Icons.add),
           ),
         ],
       ),
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -9,7 +9,7 @@ class AppDrawer extends StatelessWidget {
       child: Column(
         children: [
           AppBar(
-            title: Text('Hello man'),
+            title: Text('Menu'),
             automaticallyImplyLeading: false,
           ),
           Divider(),

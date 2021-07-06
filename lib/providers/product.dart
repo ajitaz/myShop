8061c06e18777eb96cf2af1b612679dfb08bd919
ajitaz <azalbert.ab@gmail.com>
2021-07-07 00:25:06 +0545

    patch toggle favorite

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -8,16 +11,38 @@ class Product with ChangeNotifier {
   final String imageUrl;
   bool isFavorite;
 
-  Product(
-      {required this.id,
-      required this.title,
-      required this.description,
-      required this.imageUrl,
-      required this.price,
-      this.isFavorite = false});
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.price,
+    this.isFavorite = false,
+  });
 
-  void toggleFavoriteStatus() {
+  void _setFavoriteStatus(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://flutterproject-e1375-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json';
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavoriteStatus(oldStatus);
+      }
+    } catch (error) {
+      _setFavoriteStatus(oldStatus);
+    }
   }
 }

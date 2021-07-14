709b621be595f5be29ce839859292662cc311b3f
ajitaz <azalbert.ab@gmail.com>
2021-07-14 20:24:14 +0545

    AnimationBuilder & AnimationContainer

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -100,8 +100,8 @@ class _AuthCardState extends State<AuthCard>
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
-  AnimationController? _controller;
-  Animation<Size>? _heightAnimation;
+  late AnimationController _controller;
+  late Animation<Size> _heightAnimation;
 
   @override
   void initState() {
@@ -115,16 +115,15 @@ class _AuthCardState extends State<AuthCard>
         .animate(
       CurvedAnimation(
         curve: Curves.fastOutSlowIn,
-        parent: _controller as Animation<double>,
+        parent: _controller,
       ),
     );
-    _heightAnimation!.addListener(()=>setState((){}));
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller!.dispose();
+    _controller.dispose();
   }
 
   void _showErrorDialog(String message) {
@@ -196,12 +195,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      _controller!.forward();
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _controller!.reverse();
+      _controller.reverse();
     }
   }
 
@@ -213,11 +212,14 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation!.value.height,
-        constraints:
-            BoxConstraints(minHeight:  _heightAnimation!.value.height),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+        ),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
